0af455dfb781cf33fba7a4462194c18eb6b8ad5c
AlexandraSkvortsova <45507669+AlexandraSkvortsova@users.noreply.github.com>
2020-04-15 15:53:37 +0300

    создание задания 8

---
+++ src/Zadanie8.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+public class Zadanie8 {
+    public static void main(String[] args)
+    {
+
+        for (int i=1; i<=50; i++) {
+            System.out.print(i+" ");
+        }
+
+    }
+}
