bf78518d3fc592ac214930ba80bf95b328c7552b
AlexandraSkvortsova <45507669+AlexandraSkvortsova@users.noreply.github.com>
2020-04-15 16:58:07 +0300

    создание задания 9 (массив)

---
+++ src/Zadanie9.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+
+public class Zadanie9 {
+    public static void main(String[] args)
+    {
+
+        System.out.println("Размер массива ввести через enter: ");
+        Scanner n= new Scanner(System.in);
+        String num = n.nextLine(); // считать с консоли
+
+
+        Scanner n2= new Scanner(System.in);
+        String num2 = n2.nextLine(); // считать с консоли
+        int x = Integer.parseInt(num, 10);
+        int y = Integer.parseInt(num2, 10);
+
+int mass[][]=new int[x][y];
+// Вввод элементов массива - заполнение массива
+for (int i=0; i<x;i++)
+{ for (int j=0; j<y;j++)
+    {
+        System.out.println("введите элемент массива: mass["+i+"]["+j+"]");
+        Scanner t= new Scanner(System.in);
+        String f = t.nextLine(); // считать с консоли
+        int number = Integer.parseInt(f, 10);
+       mass[i][j]=number ;
+    };
+}
+        for (int i=0; i<x;i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                System.out.println("элемент массива: mass["+i+"]["+j+"]"+"="+mass[i][j]*2);
+            }
+        }
+    }
+    }
