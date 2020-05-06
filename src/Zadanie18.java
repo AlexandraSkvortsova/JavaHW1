308ea41c35ef9a687343faf34c69c7f88f7a1b32
AlexandraSkvortsova <45507669+AlexandraSkvortsova@users.noreply.github.com>
2020-05-06 21:52:41 +0300

    задание 18 + перезаписанный файл

--- src/Zadanie18.java
+++ src/Zadanie18.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 //import java.util.Scanner;
 
 /*Необходимо написать программу, которая будет выводить на экран текстовые данные из файла .txt,
@@ -12,6 +9,7 @@ public class Zadanie18 {
     public static void main(String[] args) {
 
         String str;
+        //Считаем файл и выведем текст
         try (BufferedReader br = new BufferedReader(new FileReader("HomeWork16.txt"))) {
             while ((str = br.readLine()) != null) {
                 System.out.println(str);
@@ -22,5 +20,23 @@ public class Zadanie18 {
         } catch (IOException e) { // не понимаю почему, но без этого не работает
             e.printStackTrace();
         }
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Чтобы закончить ввод нажми enter и напиши: 'end'");
+        try (FileWriter fw=new FileWriter("HomeWork16.txt"))
+        {
+            do
+            { System.out.println(": ");
+                str=br.readLine();
+                if (str.compareTo("end")==0) break;
+                str =str+"\r\n";
+                fw.write(str); // записываем в файл
+            }
+            while (str.compareTo("end")!=0);
+
+        } catch (IOException e) {
+            System.out.println("Ошибка ввода ");
+        }
+
     }
 }
